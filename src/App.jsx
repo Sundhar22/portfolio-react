fa7b6f1850d39f1e9f2eb7d75311246d7ae6b737
Sundhar22 <mohanasundharamofficial@gmail.com>
2024-06-19 23:24:29 +0530

    Revert "chore: Update Services component CSS styles for responsiveness"
    This reverts commit 23d13d77afaf873c17e2e9a389601d937d50dd59.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+
+import './App.css';
+import About from './components/about/About';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import Services from './components/services/Services';
+import Skill from './components/skill/Skill';
+
 import "./App.css";
 import About from "./components/about/About";
 import Header from "./components/header/Header";
@@ -10,14 +18,12 @@ import Skill from "./components/skill/Skill";
 function App() {
   return (
     <>
-      <Header />
-      <main className="main">
-        <Home />
-        <About />
-        <Skill />
-        <Services />
-        <Qualification />
-        <Projects/>
+      <Header/>
+      <main className='main'>
+      <Home/>
+      <About/>
+      <Skill/>
+      <Services/>
       </main>
     </>
   );
